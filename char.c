04587d31f5e30a2b175362356473fa49db3149ab
NischayKG <1705421@kiit.ac.in>
2019-10-05 13:23:22 +0530

    char.c

--- char.c
+++ char.c
@@ -6,8 +6,8 @@ int main()
     char c;
     printf("enter a char !\n");
     do{
-        c=getch();
-        printf("\nyou enter char %c = %d =%x",c,c,c);
-    }while(c!=(char)13);
+       scanf(" %c", &c);
+        printf("\nyou enter char %c = %d =%x\n",c,c,c);
+    }while(c=='a'||c=='b'||c=='c'||c=='d'||c=='e'||c=='f'||c=='g'||c=='h'||c=='i'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='o'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='u'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z'||c=='A'||c=='B'||c=='C'||c=='D'||c=='E'||c=='F'||c=='G'||c=='H'||c=='I'||c=='J'||c=='K'||c=='L'||c=='L'||c=='M'||c=='N'||c=='P'||c=='Q'||c=='R'||c=='S'||c=='T'||c=='U'||c=='V'||c=='W'||c=='X'||c=='Y'||c=='Z');
     return 0;
 }
