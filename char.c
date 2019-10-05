ac3cc349a223ef20fe1e646efda04a48eb83ddea
NischayKG <1705421@kiit.ac.in>
2019-10-05 13:26:21 +0530

    char.c

--- char.c
+++ char.c
@@ -4,8 +4,9 @@
 int main()
 {
     char c;
-    printf("enter a char !\n");
+    
     do{
+       printf("enter a char =");
        scanf(" %c", &c);
         printf("\nyou enter char %c = %d =%x\n",c,c,c);
     }while(c=='a'||c=='b'||c=='c'||c=='d'||c=='e'||c=='f'||c=='g'||c=='h'||c=='i'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='o'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='u'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z'||c=='A'||c=='B'||c=='C'||c=='D'||c=='E'||c=='F'||c=='G'||c=='H'||c=='I'||c=='J'||c=='K'||c=='L'||c=='L'||c=='M'||c=='N'||c=='P'||c=='Q'||c=='R'||c=='S'||c=='T'||c=='U'||c=='V'||c=='W'||c=='X'||c=='Y'||c=='Z');
